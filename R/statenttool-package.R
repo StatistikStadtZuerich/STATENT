a4aa1a4b458691aad9db0aa247a1a76d61def04a
Harder Maike (SSZ) <maike.harder@zuerich.ch>
2024-08-06 11:19:06 +0200

    Package Setup

--- R/statenttool-package.R
+++ R/statenttool-package.R
@@ -1,6 +1,22 @@
 #' @keywords internal
 "_PACKAGE"
-
+#'
+#' @importFrom data.table fread
+#' @import dplyr 
+#' @importFrom golem add_resource_path activate_js favicon bundle_resources
+#' @import icons
+#' @import jpeg
+#' @importFrom knitr kable
+#' @import openxlsx
+#' @import reactable
+#' @importFrom readxl read_excel
+#' @import shiny
+#' @importFrom shinycssloaders withSpinner
+#' @import shinyjs
+#' @importFrom tidyr pivot_longer
+#' @import zuericssstyle
+#' 
+#' 
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
