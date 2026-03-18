6eae338f422c67d37f7557051cc21e4045a778e1
Metzler Aline (SSZ) <Aline.Metzler@zuerich.ch>
2026-03-18 16:18:17 +0100

    delete css, add ssz_page, add br() before results

--- R/app_ui.R
+++ R/app_ui.R
@@ -17,26 +17,25 @@ app_ui <- function(request) {
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # Your application UI logic
-    add_dependencies(fluidPage(
-      sidebarPanel(
-        mod_input_ui(
-          id = "input_widgets",
-          choices_inputs = choices_inputs
-        ),
-        conditionalPanel(
-          condition = "input.query_start == 0",
-          sszActionButton("query_start", "Abfrage starten")
-        ),
-        conditionalPanel(
-          condition = "input.query_start",
-          mod_download_ui("download")
-        )
+    add_dependencies(ssz_page(
+
+      mod_input_ui(
+        id = "input_widgets",
+        choices_inputs = choices_inputs
       ),
-      mainPanel(
-        conditionalPanel(
-          condition = "input.query_start",
-          mod_result_ui(id = "results")
-        )
+      conditionalPanel(
+        condition = "input.query_start == 0",
+        sszActionButton("query_start", "Abfrage starten")
+      ),
+      conditionalPanel(
+        condition = "input.query_start",
+        mod_download_ui("download")
+      ),
+   
+      br(),
+      conditionalPanel(
+        condition = "input.query_start",
+        mod_result_ui(id = "results")
       )
     ))
   )
